317cc3857ef03ac68d1286729f1a5405468b7b19
nargis21 <nargisakther1933@gmail.com>
2024-07-09 11:06:24 +0600

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import { Button } from "./components/ui/button"
+
+
+function App() {
+
+  return (
+    <div>
+      <h1>Redux Todo</h1>
+      <Button>Button</Button>
+    </div>
+  )
+}
+
+export default App
